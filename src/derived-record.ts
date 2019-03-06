6de765edf6405049fccc491b7aeba94f315e750d
Eric Crosson <eric.s.crosson@utexas.edu>
2019-03-06 13:30:56 -0600

    Generate typescript package

---
+++ src/derived-record.ts
@@ -0,0 +1,12 @@
+/**
+ * derived-record
+ * Interface for data derived from time-series record data
+ */
+
+
+/**
+ * TODO: document
+ */
+export default function derivedRecord() {
+    // TODO: implement
+}
